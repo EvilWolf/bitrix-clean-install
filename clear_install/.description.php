f5aba1559b0a996683918479c88f8eff3497294e
Evil_Wolf <wolfofdeath@yandex.ru>
2017-07-17 13:03:12 +0300

    Empty Wizzard

---
+++ clear_install/.description.php
@@ -0,0 +1,18 @@
+<?
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+
+$arWizardDescription = Array(
+    "NAME" => "Чистая установка", 
+    "DESCRIPTION" => "Мастер по созданию чистой копии битрикс", 
+    "IMAGE" => "images/wizard_image.png",
+    "WIZARD_TYPE" => "INSTALL",
+    "VERSION" => "0.0.1",
+    "COPYRIGHT" => "Петушиная организация",
+    "DEPENDENCIES" => Array(),
+    "TEMPLATES" => Array(),
+    "STEPS_SETTINGS" => Array(
+        "WELCOME" => Array(
+            "CONTENT" => "Добро пожаловать в мастер создания чистого сайта",
+        ),
+    )
+);
